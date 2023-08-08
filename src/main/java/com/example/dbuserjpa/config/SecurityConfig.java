1c654eb2a3f26ef8f912a5bf367d210a0e77f01a
Andy Tael <andy.tael@yahoo.com>
2023-08-08 09:21:00 -0500

    Stuff

--- src/main/java/com/example/dbuserjpa/config/SecurityConfig.java
+++ src/main/java/com/example/dbuserjpa/config/SecurityConfig.java
@@ -3,20 +3,13 @@ package com.example.dbuserjpa.config;
 import com.example.dbuserjpa.userdetailservice.JpaUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,24 +24,6 @@ public class SecurityConfig {
         this.jpaUserDetailsService = jpaUserDetailsService;
     }
 
-    /*
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests(auth -> auth
-                        .requestMatchers(HttpMethod.GET,"/ping/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ROLE_USER", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/error/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .userDetailsService(jpaUserDetailsService)
-                .httpBasic(withDefaults())
-                .csrf(CsrfConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
-    }
-*/
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -62,43 +37,6 @@ public class SecurityConfig {
                 .userDetailsService(jpaUserDetailsService)
                 .build();
     }
-/*
-    @Bean
-    public InMemoryUserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("atael")
-                        .password("{noop}password")
-                        .authorities("read")
-                        .build()
-        );
-    }
-*/
-    /*
-    @Bean
-	SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-		return http
-				.securityMatcher(new AntPathRequestMatcher("/api/**"))
-				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .userDetailsService(jpaUserDetailsService)
-                .csrf(AbstractHttpConfigurer::disable)
-				.httpBasic(withDefaults())
-				.build();
-	}
-*/
-/*
-    @Bean
-    SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .securityMatcher("/api/**")
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .userDetailsService(jpaUserDetailsService)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(withDefaults())
-                .build();
-    }
-*/
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/com/example/dbuserjpa/model/User.java
+++ src/main/java/com/example/dbuserjpa/model/User.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
 
     @Id @GeneratedValue
@@ -15,53 +23,10 @@ public class User {
     private String password;
     private String roles;
 
-    public User() {}
-
     public User(String username, String password, String roles) {
         this.username = username;
         this.password = password;
         this.roles = roles;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
-
-    @Override
-    public String toString() {
-        return "SecurityUser{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", roles='" + roles + '\'' +
-                '}';
-    }
 }
